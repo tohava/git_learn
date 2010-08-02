29b5b68ff58127e51169e127243f5d2f180fe426
Tohava <tohava@tohava-laptop.(none)>
2010-08-02 16:08:14 +0300

    Support for operators
    Currently, we have addition and multiplication

--- main.ml
+++ main.ml
@@ -1,4 +1,18 @@
+let mult a b = a * b;;
+let add a b = a + b;;
+
+let id a = a
+let read_operator ()  =
+  print_string "Please enter an operator\n";
+  flush stdout;
+  let op = Scanf.scanf "%c" id in
+  match op with
+	'*' -> mult
+  | '+' -> add;;
+
+	  
 print_string "Welcome to the calculator program\n";;
 let x = read_int () in
 let y = read_int () in
-print_int (x + y)
+let op = read_operator() in
+print_int (op x y)
