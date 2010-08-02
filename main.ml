a18aeb3c6265414bab9696eab88ae88b90d30d73
Tohava <tohava@tohava-laptop.(none)>
2010-08-02 16:37:53 +0300

    added splash

--- main.ml
+++ main.ml
@@ -1,4 +1,4 @@
-print_string "Welcome to the calculator program\n";;
+print_string "SPLASH!!! Welcome to the calculator program\n";;
 let x = read_int () in
 let y = read_int () in
 print_int (x + y)
